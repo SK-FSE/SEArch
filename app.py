5c72eef1e00f9430ed1e9b5aaf58f371a38828b3
Ildar Gabdrakhmanov <gab.ild@yandex.ru>
2020-10-17 19:22:40 +0300

    cd

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from src import query_processor
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -16,6 +17,39 @@ def search(query=None):
 def root(query=None):
     return 'go to /search/smth'
 
+
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        
+        try:
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+ 
+                # Вставляем множество данных в таблицу используя безопасный метод "?"
+                albums = [('1', 'test 1'),
+                          ('2', 'test 2'),
+                          ('3', 'test 3'),
+                          ('4', 'test 4')]
+ 
+                cur.executemany("INSERT INTO albums VALUES (?,?)", albums)
+            
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+@app.route('/list')
+def list():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+   
+    cur = con.cursor()
+    cur.execute("select * from students")
+   
+    rows = cur.fetchall();
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host="0.0.0.0")
