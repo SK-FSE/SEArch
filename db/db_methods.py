ad627795415f680bf59b446825c90275e3fef5ad
Jull-Or <71978607+Jull-Or@users.noreply.github.com>
2020-10-18 23:18:06 +0300

    Update db_methods.py

--- db/db_methods.py
+++ db/db_methods.py
@@ -9,16 +9,16 @@ import sqlite3 as sql
 
 def set_dataset_by_id(input):
     try:
-       with sql.connect("mydatabase.db") as con:
-         cur = con.cursor()
-          if type(input) is list:
-           cur.executemany('''UPDATE datasets SET dataset = ? 
+        with sql.connect("mydatabase.db") as con:
+            cur = con.cursor()
+            if type(input) is list:
+                cur.executemany('''UPDATE datasets SET dataset = ? 
+                                    WHERE article_id=?''', input)
+                con.commit()
+            else:
+                cur.execute('''UPDATE datasets SET dataset = ? 
                                 WHERE article_id=?''', input)
-            con.commit()
-          else:
-            cur.execute('''UPDATE datasets SET dataset = ? 
-                            WHERE article_id=?''', input)
-            con.commit()
+                con.commit()
     except Exception as e:
         con.rollback()
         msg = "error in set_dataset_by_id"
@@ -28,15 +28,15 @@ def set_dataset_by_id(input):
 def set_article_description_by_id(input):
     try:
         with sql.connect("mydatabase.db") as con:
-          cur = con.cursor()
+            cur = con.cursor()
             if type(input) is list:
-              cur.executemany('''UPDATE articles SET text = ? 
-                                  WHERE id=?''', input)
-              con.commit()
+                cur.executemany('''UPDATE articles SET text = ? 
+                                    WHERE id=?''', input)
+                con.commit()
             else:
-              cur.execute('''UPDATE articles SET text = ? 
-                              WHERE id=?''', input)
-              con.commit()      
+                cur.execute('''UPDATE articles SET text = ? 
+                                WHERE id=?''', input)
+                con.commit()      
     except Exception as e:
         con.rollback()
         msg = "set_article_description_by_id"
@@ -53,7 +53,7 @@ def set_article_by_id(input):
                 con.commit()
             else:
                 cur.execute('''UPDATE articles SET title =? 
-                                    WHERE id=?''', input)
+                                WHERE id=?''', input)
                 con.commit()
     except Exception as e:
         con.rollback()
