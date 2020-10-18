0dce2ca4aceaee26c9efc23dabb689774ad10bc8
Jull-Or <71978607+Jull-Or@users.noreply.github.com>
2020-10-18 22:45:35 +0300

    Update db_methods.py

--- db/db_methods.py
+++ db/db_methods.py
@@ -4,8 +4,6 @@ input_examples: input = [('sth_1', 'id_1'),
                          ('sth_2', 'id_2'), 
                          ('sth_3', 'id_3')] 
               or input = ('sth_1', 'id_1')
-ids_example: ids = [(id_1,),(id_3,),(id_4,)] 
-          or ids = [(id_1,)]
  """
 def set_dataset_by_id(input):
     try:
@@ -16,13 +14,11 @@ def set_dataset_by_id(input):
                     cur.executemany('''UPDATE datasets SET dataset = ? 
                                         WHERE article_id=?''', input)
                     con.commit()
-                    msg = "dataset successfully set"
                 else:
                     cur.execute('''UPDATE datasets SET dataset = ? 
                                     WHERE article_id=?''', input)
                     con.commit()
-                    msg = "dataset successfully set"
-    except Set_Dataset_Error:
+    except Exception as e:
         con.rollback()
         msg = "error in set_dataset_by_id"
         print(msg)
@@ -36,13 +32,11 @@ def set_article_description_by_id(input):
                     cur.executemany('''UPDATE articles SET text = ? 
                                         WHERE id=?''', input)
                     con.commit()
-                    msg = "article description successfully set"
                 else:
                     cur.execute('''UPDATE articles SET text = ? 
                                     WHERE id=?''', input)
-                    con.commit()
-                    msg = "article description successfully set"        
-    except Set_Article_Description_Error:
+                    con.commit()      
+    except Exception as e:
         con.rollback()
         msg = "set_article_description_by_id"
         print(msg)
@@ -56,13 +50,11 @@ def set_article_by_id(input):
                     cur.executemany('''UPDATE articles SET title = ? 
                                         WHERE id=?''', input)
                     con.commit()
-                    msg = "article successfully set"
                 else:
                     cur.execute('''UPDATE articles SET title =? 
                                     WHERE id=?''', input)
                     con.commit()
-                    msg = "article successfully set"
-    except Set_Article_Error:
+    except Exception as e:
         con.rollback()
         msg = "error in set_article_by_id"
         print(msg)
@@ -73,38 +65,50 @@ def set_article_by_id(input):
 def get_article_description_by_id(ids):
     try:
             with sql.connect("mydatabase.db") as con:
+                con.row_factory = sql.Row
                 cur = con.cursor()
+                result = []
 
                 for id in ids:
                     cur.execute('''SELECT text FROM articles 
                                     WHERE id=?''', id)
-                    result = cur.fetchall()
-    except Get_Article_Description_Error:
+                    rows = cur.fetchall()
+                    result.append(rows[0]['text'])
+                return result
+    except Exception as e:
         msg = "article_description/s not found"
         print(msg)
 
 def get_article_by_id(ids):
     try:
         with sql.connect("mydatabase.db") as con:
+            con.row_factory = sql.Row
             cur = con.cursor()
+            result = []
             
             for id in ids:
                 cur.execute('''SELECT title FROM articles 
                                 WHERE id=?''', id)
-                result = cur.fetchall()
-    except Get_Article_Error:
+                rows = cur.fetchall()
+                result.append(rows[0]['title'])
+            return result
+    except Exception as e:
         msg = "article/s not found"
         print(msg)
 
 def get_dataset_by_id(ids):
     try:
         with sql.connect("mydatabase.db") as con:
+            con.row_factory = sql.Row
             cur = con.cursor()
+            result = []
 
             for id in ids:
                 cur.execute('''SELECT dataset FROM datasets 
-                                WHERE article_id=?''', id)
-                result = cur.fetchall()
-    except Get_Dataset_Error:
+                                WHERE article_id=?''', [id])
+                rows = cur.fetchall()
+                result.append(rows[0]['dataset'])
+            return result
+    except Exception as e:
         msg = "dataset/s not found"
         print(msg)
