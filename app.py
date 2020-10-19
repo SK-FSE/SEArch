b0f38f44dc9b152d57f3b2c3483cb0e172cd4a30
Misha Filitov <filitovme@mail.ru>
2020-10-19 23:08:16 +0300

    integration works

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for, \
+    session, send_from_directory, current_app, send_file
 from src import query_processor
 import sqlite3 as sql
+import os
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 # frontend simulation. Frontend sends query to get_search_result
 # get_search_result is an entrypoint for backend
-@app.route('/search/<query>')
-def search(query=None):
-    result = ' '.join(query_processor.get_search_result(query))
-    return render_template('hello.html', response=result)
+# @app.route('/search/<query>')
+# def search(query=None):
+#     result = ' '.join(query_processor.get_search_result(query))
+#     return render_template('hello.html', response=result)
 
 
-@app.route('/')
-def root(query=None):
-    return 'go to /search/smth'
-
+# @app.route('/')
+# def root(query=None):
+#     return 'go to /search/smth'
+#
 
 # tmp router to show that db works correctly
 @app.route('/list')
@@ -35,6 +39,45 @@ def list():
 
     con.close()
     return render_template('list.html', rows=rows, datasets=datasets)
+
+
+@app.route('/')
+def route():
+    return redirect('/search')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_text = request.form.get('search_text')
+        session['data'] = query_processor.get_search_result(search_text)
+        return redirect('/search/results')
+    return render_template('search.html', title="Search")
+
+
+@app.route('/search/results', methods=['GET', 'POST'])
+def search_request(): #when we searching again from results page
+
+    res = session.get('data', None)
+    search_text = request.form.get('search_text')
+    if search_text != res['query'] and search_text is not None and search_text != '':
+        res = query_processor.get_search_result(search_text)
+        session['data'] = res
+    return render_template('results.html', res=res)
+
+
+# @app.route('/search/results/<int:article_id>', methods=['GET'])
+# def show_article_info(article_id):
+#     data = session.get('data', None)
+#     article = [x for x in data['hits'] if x['id'] == article_id][0]
+#     return render_template('article_info.html', title="Article info", article=article)
+
+
+@app.route('/search/results/<int:article_id>', methods=['GET'])
+def download(article_id):
+    data = session.get('data', None)
+    uploads = os.path.join(current_app.root_path, 'Files', str(article_id)+'.pdf')
+    return send_file(uploads)
 
 
 if __name__ == '__main__':
--- src/query_processor.py
+++ src/query_processor.py
@@ -6,18 +6,28 @@ import typing as tp
 # backend entrypoint
 # backend logic should start here
 
-def get_search_result(query: str) -> tp.List[str]:
-    ml_model = model_service.Model()
-    datasets = ml_model.get_datasets_by_query(query)
-    if len(datasets) == 0:
-        response = ['404']
-    else:
-        ids = [x[0] for x in datasets]
-        response = []
-        article_dates = db_methods.get_article_date_by_ids(ids)
-        for i in range(len(ids)):
-            article_string = str(datasets[i][0]) + ';' \
-                    + str(datasets[i][1]) + ';' + str(datasets[i][2]) \
-                    + ';' + str(article_dates[i])
-            response.append(article_string)
-    return response
+def get_search_result(query: str):
+    res = {'query': query,
+               'is_filter': 0,
+               'total': 3,
+               'hits': [
+                   {'title': 'Title1', 'year': '1998', 'id': 1, 'abstract': 'text1',
+                    'article_piece': 'Hello world, You\'re rock sucker.', 'author': 'author12'},
+                   {'title': 'Title2', 'year': '2005', 'id': 2, 'abstract': 'text1', 'article_piece': 'Hello world, You\'re sock sucker.', 'author': 'author2'},
+                   {'title': 'Title3', 'year': '2005', 'id': 3, 'abstract': 'text1', 'article_piece': 'Hello world, You\'re wok sucker.', 'author': 'author3'}]}
+    return res
+#     ml_model = model_service.Model()
+#     datasets = ml_model.get_datasets_by_query(query)
+#     if len(datasets) == 0:
+#         response = ['404']
+#     else:
+#         ids = [x[0] for x in datasets]
+#         response = []
+#         [1]
+#         article_dates = db_methods.get_article_date_by_ids(ids)
+#         for i in range(len(ids)):
+#             article_string = str(datasets[i][0]) + ';' \
+#                     + str(datasets[i][1]) + ';' + str(datasets[i][2]) \
+#                     + ';' + str(article_dates[i])
+#             response.append(article_string)
+#     return response
